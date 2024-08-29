ac68477b87562892262c3b606515bc406a2e69ad
Breno Pelegrin <breno.pelegrin@usp.br>
2024-08-29 09:05:53 -0300

    ENH: add documentation for cycle1

--- cycle1/ivputils.jl
+++ cycle1/ivputils.jl
@@ -1,7 +1,35 @@
+"""
+  IVPutils
+
+A module that provides utilities (solver and numerical methods)
+to solve initial value problems (IVP).
+
+Since:
+- 08/2024
+
+Authors:
+- Breno H. Pelegrin da S. <breno.pelegrin@usp.br>
+"""
 module IVPutils
 
   export FunctionParams, MethodParams, IVPSolverParams, rk4_method, IVP_solver
 
+  """
+    MethodParams{FloatType}
+
+  Parameters for a generic numeric method for an IVP in the form:
+  - y' = f(t,y), with initial condition y(t0) = y0.
+
+  You can pass any float type (Float16, Float32, Float64)
+  in ``FloatType`` and ``type``, to achieve the desired precision.
+
+  # Parameters:
+  - f (Function): the function
+  - tn (FloatType): time in the n-step
+  - yn (FloatType): y in the n-step
+  - h (FloatType): step size
+  - type (DataType): type of the float to be used.
+  """
   struct MethodParams{FloatType}
     f::Function
     tn::FloatType
@@ -10,6 +38,24 @@ module IVPutils
     type::DataType
   end
 
+  """
+    IVPSolverParams{FloatType}
+
+  Parameters for a IVP solver that solves IVPs in the form:
+  - y' = f(t,y), with initial condition y(t0) = y0.
+
+  You can pass any float type (Float16, Float32, Float64)
+  in ``FloatType`` and ``type``, to achieve the desired precision.
+
+  # Parameters:
+  - f (Function): the function f(x,y)
+  - t0 (FloatType): time in which y(t0) = y0
+  - y0 (FloatType): initial condition
+  - tf (FloatType): final time (time to stop)
+  - h (FloatType): step size
+  - type (DataType): type of the float to be used
+  - method (Function): the method to be used
+  """
   struct IVPSolverParams{FloatType}
     f::Function
     t0::FloatType
@@ -20,6 +66,7 @@ module IVPutils
     method::Function
   end
 
+  
   function rk4_method(params::MethodParams)
     k1::params.type = params.f(params.tn, params.yn)
     k2::params.type = params.f(params.tn + 0.5*params.h, params.yn + params.h*0.5*k1)
@@ -34,12 +81,40 @@ module IVPutils
     return yn1
   end
 
+  """
+    IVP_solver(ivp_params)
+
+  Solve the initial value problem (IVP) given the
+  ivp_params (of type IVPSolverParams).
+
+  The IVP is of the form:
+  - y' = f(t,y), with initial condition y(t0) = y0.
+
+  This solver allows to use any type of float to get
+  the desired precision. When creating IVPSolverParams,
+  you can use Float64, Float32 or Float16 and pass the
+  ``type`` parameter accordingly.
+
+  # Parameters inside IVPSolverParams
+  - f (Function): the function f(x,y)
+  - t0 (FloatType): time in which y(t0) = y0
+  - y0 (FloatType): initial condition
+  - tf (FloatType): final time (time to stop)
+  - h (FloatType): step size
+  - type (DataType): type of the float to be used
+  - method (Function): the method to be used
+  """
   function IVP_solver(ivp_params::IVPSolverParams)
+    # Generates the time array from t0 to tf, with step h
     t = range(ivp_params.t0, ivp_params.tf, step=ivp_params.h)
+    # Generates an empty array with same size of t for storing the y values
     y = zeros(length(t) + 1)
+
+    # Sets up y(i=1) = y0
     y[1] = ivp_params.y0
 
-    for (i,val) in enumerate(t)
+    # For each time step in t, computes the numeric method for the i+1 step.
+    for (i, val) in enumerate(t)
       method_params = MethodParams{ivp_params.type}(
         ivp_params.f, 
         t[i],
@@ -47,9 +122,11 @@ module IVPutils
         ivp_params.h,
         ivp_params.type
       )
+      # y_(i+1) = method(params in step n)
       y[i+1] = ivp_params.method(method_params)
     end 
 
+    # Returns the filled t and y arrays
     return t, y
 
   end
--- cycle1/main.jl
+++ cycle1/main.jl
@@ -1,3 +1,20 @@
+"""
+  cycle1
+
+Module that simulates and analyzes the behaviour of a RLC
+circuit with a square-wave source.
+
+To run the main() function, call `julia main.jl` on the terminal.
+
+Dependencies:
+- Uses the IVPUtils module to solve the initial value problem (IVP)
+
+Since:
+- 08/2024
+
+Authors:
+- Breno H. Pelegrin da S. <breno.pelegrin@usp.br>
+"""
 module cycle1
   include("./ivputils.jl")
   using Printf
@@ -5,12 +22,16 @@ module cycle1
 
   function main()
 
+    # Defines the float type to use as Float64, for better precision
     type = Float64
 
+    # Defines the IVP function, of the form y' = f(x,y), with y(t0) = y0
+    # This is a function used only for testing. It isn't the RLC circuit function.
     function func(t, y)
       return t * y
     end
 
+    # Defines the initial parameters for each numerical method
     euler_params = IVPutils.IVPSolverParams{type}(
       func,                   # f
       0.0,                    # t0
@@ -30,9 +51,11 @@ module cycle1
       IVPutils.rk4_method     # method
     )
 
+    # Uses the IVP_solver from IVPUtils to solve the initial value problem.
     t_euler, y_euler = IVPutils.IVP_solver(euler_params)
     t_rk4, y_rk4 = IVPutils.IVP_solver(rk4_params)
 
+    # Prints the results for each method.
     println("Table of solutions:")
     print("----------------------------------------------------------\n")
     print("|t\t\t\t|y(euler)\t\t|y(rk4)\n")
@@ -44,6 +67,7 @@ module cycle1
 
   end
 
+  # Only runs main() if the script is run directly by calling `julia main.jl` in terminal.
   if abspath(PROGRAM_FILE) == @__FILE__
     main()
   end
